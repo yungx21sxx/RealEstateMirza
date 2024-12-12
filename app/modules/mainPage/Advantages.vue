805a6e6908e5f772eda9ce99222051a17397a3e4
yungx666 <yungx21sxx@gmail.com>
2024-12-12 14:14:19 +0300

    Init

--- app/modules/mainPage/Advantages.vue
+++ app/modules/mainPage/Advantages.vue
@@ -4,6 +4,7 @@ import WorkIcon from "~/modules/mainPage/icons/adv/WorkIcon.vue";
 import KeyIcon from "~/modules/mainPage/icons/adv/KeyIcon.vue";
 import GraphIcon from "~/modules/mainPage/icons/adv/GraphIcon.vue";
 import SupportIcon from "~/modules/mainPage/icons/adv/SupportIcon.vue";
+
 </script>
 
 <template>
@@ -12,26 +13,26 @@ import SupportIcon from "~/modules/mainPage/icons/adv/SupportIcon.vue";
 		<p class="font-semibold text-accent">Мои Преимущества</p>
 		<h2 class="font-bold text-2xl sm:text-3xl mb-8">Почему выбирают меня?</h2>
 		
-		<div class=advs>
-			<div class="adv">
-				<WorkIcon class="adv__icon"/>
-				<h3 class="adv__title">Полный цикл работы</h3>
-				<p class="adv__text">От фотосессии до заселения клиентов.</p>
+		<div class=advantages>
+			<div class="advantage">
+				<WorkIcon class="advantage__icon"/>
+				<h3 class="advantage__title">Полный цикл работы</h3>
+				<p class="advantage__text">От фотосессии до заселения клиентов.</p>
 			</div>
-			<div class="adv">
-				<KeyIcon class="adv__icon"/>
-				<h3 class="adv__title">Гарантия безопасности</h3>
-				<p class="adv__text">Проверка арендаторов и страхование.</p>
+			<div class="advantage">
+				<KeyIcon class="advantage__icon"/>
+				<h3 class="advantage__title">Гарантия безопасности</h3>
+				<p class="advantage__text">Проверка арендаторов и страхование.</p>
 			</div>
-			<div class="adv">
-				<GraphIcon class="adv__icon"/>
-				<h3 class="adv__title">Высокая прибыльность</h3>
-				<p class="adv__text">Заполняемость квартир до 95%.</p>
+			<div class="advantage">
+				<GraphIcon class="advantage__icon"/>
+				<h3 class="advantage__title">Высокая прибыльность</h3>
+				<p class="advantage__text">Заполняемость квартир до 95%.</p>
 			</div>
-			<div class="adv">
-				<SupportIcon class="adv__icon"/>
-				<h3 class="adv__title">Поддержка 24/7</h3>
-				<p class="adv__text">Всегда на связи с клиентами.</p>
+			<div class="advantage">
+				<SupportIcon class="advantage__icon"/>
+				<h3 class="advantage__title">Поддержка 24/7</h3>
+				<p class="advantage__text">Всегда на связи с клиентами.</p>
 			</div>
 		</div>
 	
@@ -40,14 +41,14 @@ import SupportIcon from "~/modules/mainPage/icons/adv/SupportIcon.vue";
 
 <style scoped lang="scss">
 
-.advs {
+.advantages {
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(273px, 1fr));
 	gap: 16px;
 	justify-content: center;
 }
 
-.adv {
+.advantage {
 	@apply rounded-md border border-gray-200 px-4 py-6 text-center flex flex-col items-center w-full;
 
 	&__title {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     '@nuxtjs/google-fonts',
     '@nuxt/image',
     'vue-yandex-maps/nuxt',
-    // 'nuxt-delay-hydration',
+    'nuxt-delay-hydration',
   ],
   routeRules: {
     '/': {prerender: true}
@@ -47,12 +47,12 @@ export default defineNuxtConfig({
     server: process.env.NODE_ENV === 'development',
     client: process.env.NODE_ENV === 'development'
   },
-  // delayHydration: {
-  //   // enables nuxt-delay-hydration in dev mode for testing
-  //   // NOTE: you should disable this once you've finished testing, it will break HMR
-  //   debug: process.env.NODE_ENV === 'development',
-  //   mode: 'init'
-  // },
+  delayHydration: {
+    // enables nuxt-delay-hydration in dev mode for testing
+    // NOTE: you should disable this once you've finished testing, it will break HMR
+    debug: process.env.NODE_ENV === 'development',
+    mode: 'init'
+  },
   // vitalizer: {
   //   // Remove the render-blocking entry CSS
   //   disableStylesheets: 'entry',
@@ -84,6 +84,35 @@ export default defineNuxtConfig({
   //     // Other optimization options
   //   }
   // },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ru'
+      },
+      link: [
+        {rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png'},
+        {rel: 'icon', sizes: "32x32", type: 'image/png', href: '/favicon-32x32.png'},
+        {rel: 'icon', sizes: "16x16", type: 'image/png', href: '/favicon-16x16.png'},
+        {rel: 'icon',  type: 'image/x-icon', href: '/favicon.ico'},
+        {rel: 'manifest', href: '/site.webmanifest'},
+        {rel: 'mask-icon', color: "#ffffff", href: '/safari-pinned-tab.svg'},
+
+      ],
+      meta: [
+        {name: "msapplication-TileColor", content: "#ffffff"},
+        {name: "theme-color", content: "#ffffff"},
+        {charset: 'utf-16'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {name: 'format-detection', content: 'telephone=no'},
+        {
+          name: 'description',
+          content: 'Я занимаюсь сдачей квартир посуточно уже более 5 лет, помогая собственникам получать стабильный доход, а арендаторам — находить удобное жилье. Мой опыт и внимание к деталям обеспечивают высокую заполняемость и безопасное сотрудничество.'
+        },
+
+      ],
+      title: 'Сдать квартиру посуточно в Москве с гарантией надежности и максимальной доходности.'
+    },
+  },
 
   compatibilityDate: '2024-11-23'
 })
